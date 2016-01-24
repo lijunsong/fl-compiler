3e35e35b8255e3df684fac4f07b9b3af6bc9ff22
lijunsong <ljs.darkfish@gmail.com>
2016-01-24 12:03:04 -0800

    start ir

---
+++ src/frameStack.ml
@@ -0,0 +1,7 @@
+module Temp : sig
+  type label
+  type temp
+end = struct
+  type label = string
+  type temp = int
+end
---
+++ src/ir.ml
@@ -0,0 +1,26 @@
+(** This module defines the Intermediate Representation of the tiger
+ * compiler. *)
+
+open FrameStack
+
+type exp =
+  | CONST of int
+  | NAME of Temp.label
+  | TEMP of Temp.temp
+  | BINOP of binop * exp * exp
+  | MEM of exp (** memory operation. MEM means store only on the left side of MOVE; it means fetch in other cases *)
+  | CALL of exp * exp list (** [function arguments] *)
+  | ESEQ of stmt * exp (** evaluate stmt and then return the result of exp *)
+ and stmt =
+   | MOVE of exp * exp
+   | EXP of exp
+   | JUMP of exp * Temp.label list (** jump to exp, which has a possible location specified in the list *)
+   | CJUMP of relop * exp * exp * Temp.label * Temp.label (** CJUMP(o, e1, e2, t, f), evaluate o(e1, e2), jump to t if true, f if false*)
+   | SEQ of stmt * stmt
+   | LABEL of Temp.label
+ and binop =
+   | PLUS | MINUS | MUL | DIV
+   | AND | OR | LSHIFT | RSHIFT | ARSHIFT | XOR
+ and relop =
+   | EQ | NE | LT | GT | LE | GE
+   | ULT | ULE | UGT | UGE
