9cc1829fdfac31898a91483ef5b790f06f26b4ae
lijunsong <ljs.darkfish@gmail.com>
2016-03-08 18:38:39 -0800

    work on flow

--- backend/flow.ml
+++ backend/flow.ml
@@ -12,12 +12,73 @@ type node = {
 
 type flowgraph = node list
 
-let instr2node instr = match instr with
-  | OP (ass, def, use, jump) ->
-    {def; use; ismove=false; succ=[]; pred=[]; live_out=[]}
-  | LABEL (ass, l) ->
-    {def=[]; use=[]; ismove=false; succ=[]; pred=[]; live_out=[]}
-  | MOVE (ass, dst, src) ->
-    {def=[dst]; use=[src]; ismove=false; succ=[]; pred=[]; live_out=[]}
+module LabelMap = Map.Make(struct
+    type t = Temp.label
+    let compare = compare
+  end)
 
+(** Convert instructions to a directed graph.
+ *
+ * Note: theoretically, all instruction except label will always have
+ * its previous instruction as pred, but we might sometimes want to
+ * feed a non-canonical IR. So I'll always check whether previous IR
+ * falls through. *)
 let instrs2graph instrs =
+  (* iterate all instrs and construct a node for each
+     instruct. Map label to its position (index) for future
+     reference. *)
+  let rec conv idx instrs nodes map : node list * LabelMap.t =
+    match instrs with
+    | [] -> List.rev nodes, map
+    | OP (ass, def, use, _) :: rest ->
+      let nodes' = {def; use; ismove=false;
+                    succ=[]; pred=[]; live_out=[]} :: nodes in
+      conv (idx + 1) rest nodes' map
+    | LABEL (ass, l) ->
+      let nodes' = {def=[]; use=[]; ismove=false;
+                    succ=[]; pred=[]; live_out=[]} :: nodes in
+      conv (idx + 1) rest nodes' (LabelMap.add l idx labelMap)
+    | MOVE (ass, dst, src) ->
+      let nodes' = {def=[dst]; use=[src]; ismove=false;
+                    succ=[]; pred=[]; live_out=[]} :: nodes in
+      conv (idx+1) rest nodes' map
+  in
+  (* get nodes and labelMap *)
+  let nodes, labelMap = conv 0 instrs [] LabelMap.empty in
+  let total = List.length nodes in
+  (* now, a few helper functions to fetch nodes *)
+  let get_node i = List.nth nodes i in
+  let add_pred node pred = node.pred <- pred :: node.pred in
+  (* OK. start to fill in succ and pred details *)
+  let connect idx instr node =
+    (* update this instruction's pred if previous instr falls
+       through *)
+    begin if idx <> 0 then
+        match List.nth instrs (idx-1) with
+        | OP (_, _, _, Some(_)) -> ()
+        | _ -> add_pred node (List.nth nodes (idx-1))
+    end;
+    (* update succ *)
+    let update_succ jmps = match jmps with
+      | Some(labs) -> (* get succ from jump *)
+        let lab_idx = List.map (fun l -> LabelMap.find l labelMap) labs in
+        (* get this instructions succ *)
+        let succ = List.map get_node lab_idx in
+        (* update succ *)
+        node.succ <- succ;
+        (* update succ's pred *)
+        List.iter (fun n -> add_pred n node) succ
+      | None when idx = total-1 -> ()
+      | None ->
+        let succ = get_node (idx+1) in
+        (* succ is its next instruction *)
+        node.succ <- [succ];
+        (* update succ's pred*)
+        add_pred succ node
+    in
+    match instr with
+    | OP (_, _, _, jmps) -> update_succ jmps
+    | _ -> update_succ None
+  in
+  List.mapi (fun idx (instr, node) -> connect idx instr node)
+    (List.combine instrs nodes)
